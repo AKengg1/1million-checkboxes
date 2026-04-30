f96a0d3aeaf5e7492bdc3d374dabb4960f62d895
AKengg1 <ak72532961@gmail.com>
2026-04-30 23:45:05 +0530

    build socket connections

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-import http from 'http';
-import path from "path"
+import http from "http";
+import path from "path";
 
-import express from "express"
-import { Server } from 'socket.io';
-import { Socket } from 'dgram';
+import express from "express";
+import { Server } from "socket.io";
+import { Socket } from "dgram";
 
-async function startServer() {
-const app =express()
-const server = http.createServer(app)
-const PORT =process.env.PORT ?? 8000
+const box =100
 
-
-const io = new Server()
-io.attach(server)
-
-io.on("connection",(socket)=>{
-console.log("socket connected", {id:socket.id})
-})
-
-
-app.use(express.static(path.resolve('public')))
-
-app.get("/health",(req,res)=>{
-    res.json({status:'ok'})
-})
-
-server.listen(PORT,(req, res)=>{
-    console.log(`Server running at http://localhost:${PORT}`)
-})
+const state={
+    checkboxes:new Array(box).fill(false)
 }
 
-startServer()
+
+async function startServer() {
+  const app = express();
+  const server = http.createServer(app);
+  const PORT = process.env.PORT ?? 8000;
+
+  const io = new Server();
+  io.attach(server);
+
+  io.on("connection", (socket) => {
+    console.log("socket connected", { id: socket.id });
+
+    socket.on("client:checkbox:changes", (data)=>{
+        console.log(`[Socket:${socket.id}]:client:checkbox:changes`, data)
+        io.emit("server:checkbox:change", data)
+        state.checkboxes[data.index]=data.checked
+    })
+  });
+
+  app.use(express.static(path.resolve("public")));
+
+  app.get("/health", (req, res) => {
+    res.json({ status: "ok" });
+  });
+
+  app.get("/checkboxes",(req,res)=>{
+    return res.json({checkbox:state.checkboxes})
+  })
+
+  server.listen(PORT, (req, res) => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
+
+startServer();
