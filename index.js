4e33758f99c0b5f612396e378677f68af46fb2a6
AKengg1 <ak72532961@gmail.com>
2026-04-30 14:39:58 +0530

    frontend

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import http from 'http';
-import express from "express"
 import path from "path"
+
+import express from "express"
+import { Server } from 'socket.io';
+import { Socket } from 'dgram';
 
 async function startServer() {
 const app =express()
 const server = http.createServer(app)
 const PORT =process.env.PORT ?? 8000
+
+
+const io = new Server()
+io.attach(server)
+
+io.on("connection",(socket)=>{
+console.log("socket connected", {id:socket.id})
+})
+
 
 app.use(express.static(path.resolve('public')))
 
